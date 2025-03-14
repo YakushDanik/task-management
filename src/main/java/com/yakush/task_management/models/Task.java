9949b1047debaa0efc5f02f5f2e90aad9220c443
YakushDanik <116085546+YakushDanik@users.noreply.github.com>
2025-03-14 15:51:11 +0300

    Update Task.java

--- src/main/java/com/yakush/task_management/models/Task.java
+++ src/main/java/com/yakush/task_management/models/Task.java
@@ -1,16 +1,14 @@
 package com.yakush.task_management.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
